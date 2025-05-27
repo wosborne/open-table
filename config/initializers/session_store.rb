8fbb335b75b5f69bd7cd3fcdb2f2df096ae92e14
William Osborne <wosborne@live.co.uk>
2025-05-27 16:32:46 +0100

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,1 @@
-Rails.application.configure do
-  # Enable session store for the application
-  config.session_store :cookie_store, key: "phone_fliprr", domain: :all
-end
+Rails.application.config.session_store :cookie_store, key: "_phone_fliprr_session"
